cabd94406be2d996aa26a8693b622900f251d918
pabl0ide <46255367+pabl0ide@users.noreply.github.com>
2019-01-06 20:31:25 -0800

    Update runpchiprepair.sh

--- runpchiprepair.sh
+++ runpchiprepair.sh
@@ -3,8 +3,11 @@ echo Instalando Aplicaciones...
 sudo apt-get update
 sudo apt-get install android-tools-backup sunxi-tools u-boot-tools
 echo Listo
+echo Descarge los archivos de https://drive.google.com/drive/folders/1BPfAo4e1GDnaW6P-zCEOKxqW4_y6TO3c?usp=sharing y guardelos en HOME/CHIP
+read -rsp $'Presione enter para continuar...\n'
+sudo whoami
 cd /CHIP/CHIP-tools
 
-echo Conecta PocketCHIP...
+echo Conecta el PocketCHIP a la computadora...
 read -rsp $'Presione enter para continuar...\n'
 FEL='sudo sunxi-fel' FASTBOOT='sudo fastboot' SNIB=false ./chip-update-firmware.sh -L ../stable-pocketchip-b126
