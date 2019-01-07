5e8ab78fb27b7a62258b26b977c706a02dbacc71
pabl0ide <46255367+pabl0ide@users.noreply.github.com>
2019-01-06 19:35:44 -0800

    Add files via upload

---
+++ runpchiprepair.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo Instalando Aplicaciones...
+sudo apt-get install android-tools-backup sunxi-tools u-boot-tools
+echo Listo
+cd /CHIP/CHIP-tools
+
+echo Conecta PocketCHIP...
+read -rsp $'Press enter to continue...\n'
+FEL='sudo sunxi-fel' FASTBOOT='sudo fastboot' SNIB=false ./chip-update-firmware.sh -L ../stable-pocketchip-b126
