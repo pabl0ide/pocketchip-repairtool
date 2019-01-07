a96ff43e4c09e83d1aa03dc1b48ab6e34285b992
pabl0ide <46255367+pabl0ide@users.noreply.github.com>
2019-01-06 20:09:57 -0800

    Update runpchiprepair.sh

--- runpchiprepair.sh
+++ runpchiprepair.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 cd
 echo Instalando Aplicaciones...
 sudo apt-get update
@@ -7,5 +6,5 @@ echo Listo
 cd /CHIP/CHIP-tools
 
 echo Conecta PocketCHIP...
-read -rsp $'Press enter to continue...\n'
+read -rsp $'Presione enter para continuar...\n'
 FEL='sudo sunxi-fel' FASTBOOT='sudo fastboot' SNIB=false ./chip-update-firmware.sh -L ../stable-pocketchip-b126
